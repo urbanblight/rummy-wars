110a3896b85d7ac997181c754b6fc5d5f2974460
Matt Urban <38117772+urbanblight@users.noreply.github.com>
2026-09-25 22:12:59 -0700

    move install.sh to setup.sh

--- install.sh
+++
@@ -1,9 +0,0 @@
-python3 -m venv .venv/
-source .venv/bin/activate
-python -m pip install --upgrade pip
-python -m pip install -r requirements.txt
-python -m pip install -r requirements-dev.txt
-cd docs
-make html
-cd ..
-python -m playwright install chromium
