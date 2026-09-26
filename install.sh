ed4d5a2c19634447f9765db230dec5709d80d62e
Matt Urban <38117772+urbanblight@users.noreply.github.com>
2026-09-25 21:52:19 -0700

    Add Python virtual environment setup script and update .gitignore

---
+++ install.sh
@@ -0,0 +1,9 @@
+python3.10 -m venv .venv/
+source .venv/bin/activate
+python3.10 -m pip install --upgrade pip
+python3.10 -m pip install -r requirements.txt
+python3.10 -m pip install -r requirements-dev.txt
+cd docs
+make html
+cd ..
+python3.10 -m playwright install chromium
